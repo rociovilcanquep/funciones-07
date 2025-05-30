f1af971eef34580281f723c84e6f73bffcfd6ac2
rvilcanquep <rvilcanquep@unjbg.edu.pe>
2025-05-30 18:05:52 -0500

    Mejora del Ejercicio03: inclusion de la funcion del cuadrado, círculo, error y oportunidad

--- funcion03.cpp
+++ funcion03.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 void triangulo(int a);
 int rombo(int x,int y);
+int cuadrado(int l);
 int cubo(int l);
+int circulo(int r);
+int error (int n);
+int oportunidad(int m);
 int main (){
-	int opciones, lado, dmayor, dmenor, ladoc, i=1;
-	cout<<"Ingrese 1. Area de un triangulo equilatero, 2. Area de un rombo, 3. Volumen de un cubo, 0. Salir: "<<endl;
+	int opciones, lado, dmayor, dmenor, Lado, radio, ladoc, i=1;
+	cout<<"Ingrese 1. Area de un triangulo equilatero, 2. Area de un rombo, 3. Area del cuadrado, 4. Area del circulo, 5. Volumen de un cubo, 0. Salir: "<<endl;
 	cin>>opciones;
 	system("cls");
 	while (opciones!=0 && i<3){
@@ -19,7 +23,7 @@ int main (){
 			        triangulo(lado);	
 				}
 				else {
-					cout<<"ERROR, el lado de un triangulo no puede ser negativo ni 0, vuelva a intentarlo"<<endl;
+				    oportunidad(lado);
 				}
 			    break;
 		    case 2:
@@ -27,21 +31,52 @@ int main (){
 			    cin>>dmayor;
 			    cout<<"Ingresa la diagonal menor: "<<endl;
 			    cin>>dmenor;
+			    error(dmayor);
+			    error(dmenor);
 			    if (dmayor>dmenor){
 			        rombo(dmayor,dmenor);	
 				}
 				else {
-					cout<<"ERROR, la diagonal mayor ingresada es menor que la segunda diagonal, intentelo de nuevo"<<endl;
+				    while (dmayor<=dmenor && i<3){
+					    cout<<"Error el numero ingresado para la diagonal mayor debe ser mas grande que la otra diagonal, intentelo de nuevo"<<endl;
+	                	i=i+1;
+	                	cout<<"Ingresa la diagonal mayor: "<<endl;
+		        	    cin>>dmayor;
+			            cout<<"Ingresa la diagonal menor: "<<endl;
+			            cin>>dmenor;
+	            }
+	                cout<<"Eliga nuevamente una de las opciones del menu"<<endl;	
+					
 				}
 			    break;
-		    case 3:
+			case 3:
+				cout<<"Ingresa el lado del cuadrado: "<<endl;
+				cin>>Lado;
+				if (Lado>0){
+			        cuadrado(Lado);	
+				}
+				else {
+					oportunidad(Lado);
+				}
+			    break;
+		    case 4:
+			    cout<<"Ingresa el radio del circulo: "<<endl;
+			    cin>>radio;
+			    if (radio>0){
+			        circulo(radio);	
+				}
+				else {
+					oportunidad(radio);
+				}
+		        break;
+		    case 5:
 			    cout<<"Ingresa el lado del cubo: "<<endl;
 			    cin>>ladoc;
 			    if (ladoc>0){
 			        cubo(ladoc);	
 				}
 				else {
-						cout<<"ERROR, el lado de un cubo no puede ser negativo ni 0, vuelva a intentarlo"<<endl;
+					oportunidad(ladoc);
 				}
 		        break;
 		    default:
@@ -50,7 +85,7 @@ int main (){
 	   	        break;
 	    }	
 	
-	    cout<<"Ingrese 1. Area de un triangulo equilatero, 2. Area de un rombo, 3. Volumen de un cubo, 0. Salir: "<<endl;
+	    cout<<"Ingrese 1. Area de un triangulo equilatero, 2. Area de un rombo, 3. Area del cuadrado, 4. Area del circulo, 5. Volumen de un cubo, 0. Salir: "<<endl;
 	    cin>>opciones;
 	    system("cls");
 	}
@@ -72,9 +107,44 @@ int rombo(int x,int y){
 	cout<<"El area del rombo es: "<<area1<<endl;
 }
 
+
+//Funcion área del cuadrado
+int cuadrado(int l){
+	int area2;
+	area2=pow(l,2);
+	cout<<"El area del cuadrado es: "<<area2<<endl;
+}
+
+//Función área del círculo
+int circulo(int r){
+	int area3;
+	area3=(3.14)*pow(r,2);
+	cout<<"El area del circulo es: "<<area3<<endl;
+}
+
 //Funcion volumen de un cubo
 int cubo(int l){
 	int volumen;
 	volumen=pow(l,3);
 	cout<<"El volumen del cubo es: "<<volumen<<endl;
 }
+
+//Funcion error 
+int error (int n){
+	if (n<=0){
+		cout<<"ERROR, ingrese un valor positivo, intentelo de nuevo"<<endl;
+	}
+}
+
+//Funcion oportunidad
+int oportunidad(int m){
+	int i=1;
+    while (m<=0 && i<3){
+		error(m);
+		i=i+1;
+		cout<<"Ingresa el valor (no negativos ni 0): "<<endl;
+		cin>>m;
+	}
+	cout<<"Eliga nuevamente una de las opciones del menu"<<endl;	
+}
+

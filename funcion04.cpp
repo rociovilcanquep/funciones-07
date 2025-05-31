bc2f12e260d0b752e16914e7377584a3eb7954a3
rvilcanquep <rvilcanquep@unjbg.edu.pe>
2025-05-31 12:51:18 -0500

    Mejora del Ejercicio 04: muestra de divisores del número y suma de los números ingresados

--- funcion04.cpp
+++ funcion04.cpp
@@ -1,23 +1,47 @@
 #include<iostream>
 using namespace std;
 int main (){
-	int num,n=0;
-	cout<<"Ingrese el numero:";
-	cin>>num;
-    if (num==0){
-    	cout<<"El cero no es ni un numero primo ni un numero compuesto";
+	int num, cant;
+	cout<<"¿Cuantos numeros quiere ingresar (no 0, ni negativos)?:"<<endl;
+	cin>>cant;
+    if (cant==0 && cant<0){
+    	cout<<"La cantidad debe ser positiva"<<endl;
 	}
 	else{
-        for (int i=1;i<=num;i=i+1){
-    	    if (num%i==0)
-    	    n=n+1;
+		int S=0;
+		int a=1;
+		while (a<=cant){
+			int n=0;
+			cout<<"Ingresa el numero: "<<endl;
+			cin>>num;
+			if (num==0){
+        	cout<<"El cero no es ni un numero primo ni un numero compuesto";
+        	}
+	        else{
+	        	cout<<"Divisores: "<<endl;
+			    for (int i=1;i<=num;i=i+1){
+    	            if (num%i==0){
+    	            cout<<i<<" ";
+    	            n=n+1;
+    	            }
+    	        }
+    	        if (n>2){
+	                cout<<endl<<"El numero es compuesto"<<endl;
+	            }
+            	else {
+	                cout<<endl<<"El numero es primo"<<endl;
+                }
+    	        S=S+num; 
+                a=a+1;
+	        }
 	    }
+	    cout<<"La suma es: "<<S<<endl;
     }
-	if (n>2)
-	    cout<<"El numero es compuesto";
-	else 
-	    cout<<"El numero es primo";
+    cout<<"Fin del programa";
 	return 0;
 }
+
+ 
+
 
 

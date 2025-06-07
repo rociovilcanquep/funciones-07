ed488fb9e7a57971155847c27f9aca59fd083dbf
rvilcanquep <rvilcanquep@unjbg.edu.pe>
2025-06-07 17:07:33 -0500

    Actualización: validación de entrada, suma de MCD y MCM.

--- MCDyMCM_R.cpp
+++ MCDyMCM_R.cpp
@@ -3,24 +3,72 @@ using namespace std;
 
 void MCDyMCM(int a,int b,int &mcd, int &mcm);
 
-int main (){
-	int x, y;
-	cout<<"Ingresa el primer numero: ";
-	cin>>x;
-	cout<<"Ingresa el segundo numero: ";
-	cin>>y;
-	if (x==y){
-		cout<<"MCD y MCM es: "<<x<<endl;
-	}
-	else{
-	    int rMCD, rMCM;
-	    MCDyMCM(x,y,rMCD,rMCM);
-	    cout<<"MCD: "<<rMCD<<endl;
-    	cout<<"MCM: "<<rMCM<<endl;
+int main() {
+    int cant,opcion;
+    cout << "¿Cuantas veces quiere utilizar la funcion MCM y MCD?: "<<endl;
+    cin >> cant;
+    system("cls");
+    cout<<"¿Desea sumar los resultados? (1=si, 2=no): ";
+    cin>>opcion;
+    system("cls");
+    int i=1, S1=0, S2=0;
+    switch (opcion){
+    	case 1:{
+    		while (cant!=0 && i<=cant){
+                int x, y;
+	            cout<<"Ingresa el primer numero: ";
+	            cin>>x;
+	            cout<<"Ingresa el segundo numero: ";
+	            cin>>y;
+        	        if (x==y){
+		                cout<<"MCD y MCM es: "<<x<<endl;
+		                S1=S1+x;
+		                S2=S2+x;
+	                } 
+	                else{
+	                    int rMCD, rMCM;
+	                    MCDyMCM(x,y,rMCD,rMCM);
+	                    cout<<"MCD: "<<rMCD<<endl;
+    	                cout<<"MCM: "<<rMCM<<endl;
+    	                i=i+1;
+    	                S1=S1+rMCD;
+    	                S2=S2+rMCM;
+                    }
+            }
+            cout<<"La suma de los MCD ingresados es: "<<S1<<endl;
+            cout<<"La suma de los MCM ingresados es: "<<S2<<endl;
+			break;
+		}
+		case 2:{
+			while (cant!=0 && i<=cant){
+            int x, y;
+	        cout<<"Ingresa el primer numero: ";
+	        cin>>x;
+	        cout<<"Ingresa el segundo numero: ";
+	        cin>>y;
+        	    if (x==y){
+		            cout<<"MCD y MCM es: "<<x<<endl;
+	            } 
+	            else{
+	                int rMCD, rMCM;
+	                MCDyMCM(x,y,rMCD,rMCM);
+	                cout<<"MCD: "<<rMCD<<endl;
+    	            cout<<"MCM: "<<rMCM<<endl;
+    	            i=i+1;
+                }
+            }
+            break;
+	    }
+	    default: {
+	    	cout<<"Error";
+			break;
+		}
     }
-	return 0;
+    cout<<"Fin del programa";
+    return 0;
 }
 
+//Función MCD-MCM
 void MCDyMCM(int a,int b,int &mcd, int &mcm){
 	int i;
 	i=1;
